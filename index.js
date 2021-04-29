cfa3c2c0b0cc7c8307eb51d23b671cb36064f647
syed Rizvi <rizvi5a@yahoo.com>
2021-04-28 21:04:13 -0500

    STARTED FINE GENERATION

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
 // TODO: Include packages needed for this application
-
+const inquirer =require('inquirer');
+const fs =require ('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type:"input",
+        name:"projectname",
+        message:"Enter Project name?"
+    },
+    {
+        type: "list",
+        name:"license",
+        choices:["MIT","ISC","APACHE 2.0","None"],
+        message:"Choose License for your project"
+    },
+    {
+        type:"input",
+        name:"github",
+        message:"Enter Github username :"
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(file, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+   inquirer.prompt(questions)
+   .then(function(userresponse){
+       console.log(userresponse)
+    var fileContent = `
+# Project: ${userresponse.projectname}
+### Developers Profile :https://github.com/${userresponse.github}
+## Table of Contents
+*[Description](#description)
+*[Installation](#installation)
+*[License](#license)
+*
+## Description
+${userresponse.description}
+## License
+![Github License](https://img.shields.io/badge/license-${userresponse.license}-blue.svg)
+
+`
+    console.log(fileContent)
+   })
+}
 
 // Function call to initialize app
 init();
